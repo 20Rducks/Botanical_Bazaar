df5baedff37fbc87c5b457688c40e99462de54b4
robert willcox <robducks@gmail.com>
2023-05-29 21:28:03 +0100

    Final Master Push - Routes Completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :plants, only: %i[index show new create destroy] do
-    resources :bookings, only: %i[index show new create]
+    resources :bookings, only: %i[new create]
   end
+  resources :bookings, only: %i[show index]
 end
