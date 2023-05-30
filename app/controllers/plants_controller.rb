d163c0fba9986caa5dd38e329824832cebf70d71
Rachael Miller <rachaelam6@gmail.com>
2023-05-30 15:06:43 +0100

    new, create and destroy actions added and new.html.erb created

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,3 +1,33 @@
 class PlantsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  # skip_before_action :authenticate_user!, only: :index
+  before_action :set_plant, only: [:show, :create, :destroy]
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    if @plant.save
+      redirect_to plant_path(@plant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @plant.destroy
+    redirect_to plants_path, status: :see_other
+  end
+
+  private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:species, :photos, :price, :description)
+  end
+
 end
