d83f259b3810cdc73c0bf5a13b923e3251843f9e
robert willcox <robducks@gmail.com>
2023-05-31 14:25:25 +0100

    added image upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,42 @@ require 'open-uri'
 
 puts "Cleaning database..."
 
-User.destroy_all
-Plant.destroy_all
+# User.destroy_all
+# Plant.destroy_all
 
-user = User.create!(email: 'test2@test2.com', password: '123123', password_confirmation: '123123')
-
-puts "Creating Plants"
-
-10.times do
-  plant = Plant.create!(species: "Swiss Cheese Plant", price: 150, description: "Lovely swiss cheese plant to rent, comes with a block of cheese", user_id: user.id)
-  5.times do
-    file = URI.open('https://www.gardeningknowhow.com/wp-content/uploads/2012/01/peace-lily-1.jpg')
-    plant.photos.attach(io: file, filename: "plant.jpg", content_type: 'images/jpg')
-  end
-  plant.save
+user = User.create!(email: 'tesfet2@teldst2.com', password: '123123', password_confirmation: '123123')
+plant = Plant.create!(species: "Swiss Cheese Plant", price: 150, description: "Lovely swiss cheese plant to rent, comes with a block of cheese", user_id: user.id)
+5.times do
+  file = URI.open('https://www.gardeningknowhow.com/wp-content/uploads/2012/01/peace-lily-1.jpg')
+  plant.photos.attach(io: file, filename: "plant.jpg", content_type: 'images/jpg')
 end
+plant.save
+
+# user = User.create!(email: 'tesfet2@teldst2.com', password: '123123', password_confirmation: '123123')
+# plant = Plant.create!(species: "Swiss Cheese Plant", price: 150, description: "Lovely swiss cheese plant to rent, comes with a block of cheese", user_id: user.id)
+# 5.times do
+#   file = URI.open('https://www.gardeningknowhow.com/wp-content/uploads/2012/01/peace-lily-1.jpg')
+#   plant.photos.attach(io: file, filename: "plant.jpg", content_type: 'images/jpg')
+# end
+# plant.save
 
 puts "Plants Created"
+
+# # user = User.create!(email: 'rob@rob.com', password: '123123', password_confirmation: '123123')
+# # 5.times do
+# #   plant = Plant.create!(species: "Swiss Cheese Plant", price: 150, description: "Lovely swiss cheese plant to rent, comes with a block of cheese", user_id: user.id)
+# #   5.times do
+# #     file = URI.open('https://www.gardeningexpress.co.uk/media/product/6e8/echinocactus-grusonii-golden-barrel-cactus-or-mother-in-laws-seat-c3f.jpg')
+# #     plant.photos.attach(io: file, filename: "plant.jpg", content_type: 'images/jpg')
+# #   end
+# #   plant.save
+# # end
+
+# 10.times do
+#   plant = Plant.create!(species: "Swiss Cheese Plant", price: 150, description: "Lovely swiss cheese plant to rent, comes with a block of cheese", user_id: user.id)
+#   5.times do
+#     file = URI.open('https://www.gardeningknowhow.com/wp-content/uploads/2012/01/peace-lily-1.jpg')
+#     plant.photos.attach(io: file, filename: "plant.jpg", content_type: 'images/jpg')
+#   end
+#   plant.save
+# end
